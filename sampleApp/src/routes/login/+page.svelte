9752a9fbfaa949cc0fe8cfd65de36ebc9cca769f
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-06-26 17:35:19 +0200

    Logged in dialog

--- sampleApp/src/routes/login/+page.svelte
+++ sampleApp/src/routes/login/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+    import type { ActionData } from './$types';
+
+    export let form: ActionData;
+
+	$: if (form?.success) {
+		open = true
+	}
+	let open = false;
+	let dialog: HTMLDialogElement;
+	$: if (dialog && open) dialog.showModal();
+	$: if (dialog && !open) dialog.close();
+</script>
+
 <form method="POST">
 	<label
 		>Username
@@ -9,3 +23,8 @@
 	</label>
 	<button>Login</button>
 </form>
+
+
+<dialog bind:this={dialog} on:close={() => (open = false)} on:click|self={() => dialog.close()}>
+	Logged in
+</dialog>
